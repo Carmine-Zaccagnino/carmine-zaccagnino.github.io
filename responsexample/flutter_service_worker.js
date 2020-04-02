7bd40f33323469dbaf3d6b8c9de20ee587f369da
Carmine Zaccagnino <carmine@carminezacc.com>
2020-04-02 09:24:35 +0200

    added responsive app example
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

---
+++ responsexample/flutter_service_worker.js
@@ -0,0 +1,36 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/main.dart.js": "2076b335f12048679851e83196846b49",
+"/main.dart.js.deps": "302bf7b285ed2f95540b7c35d5507b96",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
+"/index.html": "8fd236a9918dee49ab575bc436006e8c"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
